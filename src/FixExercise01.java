960b4ad02b90a8044ea2843e2e91af610d1cbb51
lucasdelacale <corp.lucasmartins@gmail.com>
2025-07-16 01:31:20 -0300

    Exercise one

---
+++ src/FixExercise01.java
@@ -0,0 +1,6 @@
+public class FixExercise01 {
+    public static void main(String[] args) {
+        // For generate structure, write 'psvm'(public static void main)
+        System.out.println("Testing public static void main");
+    }
+}
